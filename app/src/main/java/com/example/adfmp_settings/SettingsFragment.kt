87e47dfd64e438cbd22ea76177378e4267977eaf
MolodzeffDanila <russgun1@mail.ru>
2023-03-12 20:14:46 +0300

    feat: added saving data to Internal Storage

--- app/src/main/java/com/example/adfmp_settings/SettingsFragment.kt
+++ app/src/main/java/com/example/adfmp_settings/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.adfmp_settings
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -15,8 +17,10 @@ class SettingsFragment : Fragment() {
     var isVibro : Boolean = false;
     var isAutoComplete : Boolean = false;
     var isAutoLoseExit : Boolean = false;
-    var TurnTimeInMinutes : Number = 1;
+    var TurnTimeInMinutes : Int = 1;
     var CurrentWordsPackage : String = "Cities";
+
+    var pref : SharedPreferences? = null;
 
     private var _binding: FragmentSettingsBinding? = null
 
@@ -29,6 +33,14 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        pref =  this.activity?.getSharedPreferences("Settings", Context.MODE_PRIVATE)
+
+        isVibro = pref?.getBoolean("vibro", false)!!
+        isAutoComplete = pref?.getBoolean("autoCompl", false)!!
+        isAutoLoseExit = pref?.getBoolean("autoLose", false)!!
+        TurnTimeInMinutes = pref?.getInt("turnTime", 1)!!
+        CurrentWordsPackage = pref?.getString("currentPack", "Cities")!!
+
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -36,6 +48,7 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.toolbar.setNavigationOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
@@ -44,5 +57,32 @@ class SettingsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveDataBool("vibro", isVibro)
+        saveDataBool("autoCompl", isAutoComplete)
+        saveDataBool("autoLose", isAutoLoseExit)
+        saveDataInt("turnTime", TurnTimeInMinutes)
+        saveDataStr("currentPack", CurrentWordsPackage)
+    }
+
+    fun saveDataBool(key: String, value: Boolean){
+        val editor = pref?.edit()
+        editor?.putBoolean(key,value)
+        editor?.apply()
+    }
+
+    fun saveDataInt(key: String, value: Int){
+        val editor = pref?.edit()
+        editor?.putInt(key,value)
+        editor?.apply()
+    }
+
+    fun saveDataStr(key: String, value: String){
+        val editor = pref?.edit()
+        editor?.putString(key,value)
+        editor?.apply()
     }
 }
