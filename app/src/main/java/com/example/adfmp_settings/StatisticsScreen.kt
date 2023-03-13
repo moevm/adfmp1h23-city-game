dd0b74101c2f1d1f7dd4cec28d863e8d9dea020e
MolodzeffDanila <russgun1@mail.ru>
2023-03-14 00:16:04 +0300

    feat: added statistics storage to UI

--- app/src/main/java/com/example/adfmp_settings/StatisticsScreen.kt
+++ app/src/main/java/com/example/adfmp_settings/StatisticsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.adfmp_settings
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -7,18 +8,22 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.adfmp_settings.databinding.StatisticsScreenBinding
 
-
+const val winRateBotKey = "winRateBot"
+const val gamesAgainstBotKey = "gamesAgainstBot"
+const val winsAgainstBotKey = "winsAgainstBot"
 
 class StatisticsScreen : Fragment() {
     private var _binding: StatisticsScreenBinding? = null
 
-    var statisticsSorage : SharedPreferences? = null
-    var winRateBot : Int = 0
-    var gamesAgainstBot : Int = 0
-    var winsAgainstBot = 0
+    var statisticsStorage : SharedPreferences? = null
+    var winRateBot : Int = 23
+    var gamesAgainstBot : Int = 111
+    var winsAgainstBot = 31
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,17 +34,31 @@ class StatisticsScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        statisticsStorage = this.activity?.getSharedPreferences("Statistics", Context.MODE_PRIVATE)
+
+        winRateBot = statisticsStorage?.getInt(winRateBotKey,23)!!
+        gamesAgainstBot = statisticsStorage?.getInt(gamesAgainstBotKey,111)!!
+        winsAgainstBot = statisticsStorage?.getInt(winsAgainstBotKey,31)!!
+
         _binding = StatisticsScreenBinding.inflate(inflater, container, false)
         return binding.root
 
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        statisticsSorage = this.activity?.getSharedPreferences("Statistics", Context.MODE_PRIVATE)
 
+        val winRateBotText : TextView = view.findViewById(R.id.winrateagainstbotpercent)
+        winRateBotText.text = "$winRateBot%"
 
-        binding.toolbarStats.setNavigationOnClickListener {
+        val gamesAgaintsBotText : TextView = view.findViewById(R.id.gamesrateagainstbot)
+        gamesAgaintsBotText.text = "$gamesAgainstBot"
+
+        val winsAgainstBotText : TextView = view.findViewById(R.id.winrateagainstbot)
+        winsAgainstBotText.text = "$winsAgainstBot"
+
+            binding.toolbarStats.setNavigationOnClickListener {
             findNavController().navigate(R.id.action_statisticsScreen_to_FirstFragment)
         }
     }
@@ -47,5 +66,11 @@ class StatisticsScreen : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun saveDataInt(key: String, value: Int){
+        val editor = statisticsStorage?.edit()
+        editor?.putInt(key,value)
+        editor?.apply()
     }
 }
